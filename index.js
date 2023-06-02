87e95851b27c3024dfdfb038b437c3db79493efa
thanapoom21 <thanapoom.p.music@gmail.com>
2023-06-02 13:48:05 -0700

    add refactored if-else statements for readability instead of nested ternary operator.

--- index.js
+++ index.js
@@ -24,15 +24,25 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date_string?", function (req, res) {
-  let timestamp = req.params.date_string == null
-    ? new Date(Date.now())
-    : /^[0-9]*$/g.test(req.params.date_string)
-      ? new Date(parseInt(req.params.date_string, 10))
-      : new Date(req.params.date_string)
+  // let timestamp = req.params.date_string == null
+  //   ? new Date(Date.now())
+  //   : /^[0-9]*$/g.test(req.params.date_string)
+  //     ? new Date(parseInt(req.params.date_string, 10))
+  //     : new Date(req.params.date_string)
+
+  let timestamp;
+  if (req.params.date_string == null) {
+    timestamp = new Date(Date.now())
+  } else if (req.params.date_string != null && /^[0-9]*$/g.test(req.params.date_string)) {
+    timestamp = new Date(parseInt(req.params.date_string, 10))
+  } else {
+    timestamp = new Date(req.params.date_string)
+  }
+
   let unixTime = timestamp.getTime();
   res.json(
     Number.isNaN(unixTime)
-      ? { error: "Invalid Date" }
+      ? { error: "Invalid Date", message: "this code has been refactored to eliminate nested ternary operators!!" }
       : { unix: unixTime, utc: timestamp.toUTCString() }
   )
 });
