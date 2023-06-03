d54f34481796f16bf90fd35ab0b8b370ca10656c
thanapoom21 <thanapoom.p.music@gmail.com>
2023-06-03 12:33:22 -0700

    combine 2 projects, replace var with let, and implement new route for whoami

--- index.js
+++ index.js
@@ -2,28 +2,28 @@
 // where your node app starts
 
 // init project
-var express = require("express");
-var app = express();
+let express = require("express");
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
+let cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/",  (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello",  (req, res) => {
   res.json({ greeting: "hello API" });
 });
 
-app.get("/api/:date_string?", function (req, res) {
+app.get("/api/timestamp/:date_string?",  (req, res) => {
   // let timestamp = req.params.date_string == null
   //   ? new Date(Date.now())
   //   : /^[0-9]*$/g.test(req.params.date_string)
@@ -47,7 +47,15 @@ app.get("/api/:date_string?", function (req, res) {
   )
 });
 
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req["ip"],
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
