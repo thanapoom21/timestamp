11c301da408bde35827dcc8284c09f86b5fcea71
thanapoom21 <thanapoom.p.music@gmail.com>
2023-06-25 15:49:55 -0700

    add dotenv express and mongoose for users' posts and logs

--- index.js
+++ index.js
@@ -8,6 +8,31 @@ const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser")
 const dns = require('dns')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// This section is where DB code lives.
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true })
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true
+  }
+})
+
+const exerciseSchema = mongoose.Schema({
+  user_id: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date },
+})
+
+let User = mongoose.model('User', userSchema);
+let Exercise = mongoose.model('Exercise', exerciseSchema);
+
+// This is the section end line for DB code.
 
 // so that your API is remotely testable by FCC
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
@@ -76,13 +101,13 @@ app.post('/api/shorturl', (req, res) => {
   } else {
     dns.lookup(domainName, (err, address, family) => {
       if (err || !address) {
-        res.json({error: "Invalid URL"})
+        res.json({ error: "Invalid URL" })
       } else {
         let newURL = {
           original_url: url,
           short_url: urls.length
         }
-  
+
         urls.push(newURL)
         res.json(newURL)
       }
@@ -99,11 +124,90 @@ app.get('/api/shorturl/:id?', (req, res) => {
     if (long_url) {
       res.redirect(long_url);
     } else {
-      res.json({"error": "Not Found."})
+      res.json({ "error": "Not Found." })
     }
   } else {
-      res.json({"error": "Not Found."})
-    }
+    res.json({ "error": "Not Found." })
+  }
+})
+
+app.get('/api/users', async (req, res) => {
+  let users = await User.find({});
+
+  users.length == 0 ? res.json("No users found!") : res.json(users);
+})
+
+app.post('/api/users', async (req, res) => {
+  let newUser = await User.create({ username: req.body.username })
+
+  res.json({
+    username: newUser.username,
+    _id: newUser._id,
+  })
+})
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  let userID = req.params._id
+  let payloadDate = new Date(req.body.date).toDateString();
+  let now = new Date(Date.now()).toDateString()
+
+  User.findById(userID).then(userFound => {
+    Exercise.create({
+      user_id: userFound._id,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: req.body.date ? payloadDate : now
+    }).then(data => {
+      res.json({
+        "_id": userFound._id,
+        "username": userFound.username,
+        "date": data.date.toDateString(),
+        "duration": data.duration,
+        "description": data.description,
+      });
+    }).catch((err) => {
+      console.error(err)
+      err.stack
+    });
+  })
+})
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  let userID = req.params._id
+  let { from: startDate, to: endDate, limit: maximum } = req.query
+
+  if (startDate) {
+    startDate = new Date(startDate).getTime()
+  }
+
+  if (endDate) {
+    endDate = new Date(endDate).getTime()
+  }
+
+  User.findById(userID).then(userFound => {
+    Exercise.find({
+      date: {
+        "$gte": startDate || new Date('1450-1-1'),
+        "$lt": endDate || new Date('2300-1-1')
+      },
+      user_id: userID
+    }).limit(maximum)
+      .select({ user_id: 0 })
+      .then(docs => {
+        res.json({
+          "_id": userFound._id,
+          "username": userFound.username,
+          "count": docs.length,
+          "log": docs.map(doc => ({
+            "date": doc.date.toDateString(),
+            "duration": doc.duration,
+            "description": doc.description,
+          }))
+        });
+      }).catch((err) => {
+        console.error(err)
+      });
+  })
 })
 
 // listen for requests :)
